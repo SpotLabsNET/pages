2e11fc170478b4dfde2a02c6a7ceeecac2aa5458
Jevon Wright <jevon@jevon.org>
2015-05-01 14:40:33 +1200

    Arguments passed to templates are passed to sub-templates
    This means that requireTemplate() doesn't need to manually copy over any
    arguments to child requireTemplate() calls, e.g.
    
    `requireTemplate('template', array('a' => 'b', 'args' => $args) + $args)`

--- src/PageRenderer.php
+++ src/PageRenderer.php
@@ -76,6 +76,11 @@ class PageRenderer {
   static $executor = null;
 
   /**
+   * Allow arguments to be passed along to sub-templates.
+   */
+  static $argument_stack = array();
+
+  /**
    * Render the given template.
    *
    * Searches for templates within each template directory, with the following
@@ -88,10 +93,19 @@ class PageRenderer {
 
     Events::trigger('pages_template_start', $arguments + array('template' => $template));
 
+    // so sub-templates can access all parent arguments
+    if (self::$argument_stack) {
+      $arguments += self::$argument_stack[count(self::$argument_stack) - 1];
+    }
+
+    // add to argument stack
+    array_push(self::$argument_stack, $arguments);
+
     foreach (self::$templates as $dir) {
       // either include templates as direct PHP files...
       $file = $dir . "/" . $template . ".php";
       if (file_exists($file)) {
+
         // create locally scoped variables for all arguments
         foreach ($arguments as $key => $value) {
           $$key = $value;
@@ -99,6 +113,10 @@ class PageRenderer {
 
         require($file);
         Events::trigger('pages_template_end', $arguments + array('template' => $template, 'filetype' => 'php'));
+
+        // and then remove from argument stack
+        array_pop(self::$argument_stack);
+
         return;
       }
 
@@ -115,6 +133,10 @@ class PageRenderer {
           // Compiles and executes the HAML template, with variables given as second argument
           self::$executor->display($file, $arguments);
           Events::trigger('pages_template_end', $arguments + array('template' => $template, 'filetype' => 'haml'));
+
+          // and then remove from argument stack
+          array_pop(self::$argument_stack);
+
           return;
         }
       }
